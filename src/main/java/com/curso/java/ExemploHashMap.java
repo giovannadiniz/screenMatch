a9f932053ec3cd16efb5a1c909c4678791e2042d
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2025-09-27 19:58:25 -0300

    feat: exercitando

---
+++ src/main/java/com/curso/java/ExemploHashMap.java
@@ -0,0 +1,47 @@
+package com.curso.java;
+
+import java.util.*;
+
+public class ExemploHashMap {
+    public static void main(String[] args) {
+//        Map<String, Integer> usandoHashMap = new HashMap<>();
+//
+//        usandoHashMap.put("João", 25);
+//        usandoHashMap.put("Maria", 30);
+//        usandoHashMap.put("Pedro", 20);
+//
+//        int valor = usandoHashMap.get("Pedro");
+//        System.out.println("Valor da Chave Pedro: " + valor);
+//
+//        usandoHashMap.remove("Maria");
+//
+//        for (String chave : usandoHashMap.keySet()) {
+//            System.out.println("chave: " + chave);
+//            System.out.println("valor: " + usandoHashMap.get(chave));
+//        }
+
+//        -- Exercício 1 --
+
+//        List<Integer> listaInteiros = new ArrayList<>();
+//        int num3 = 3;
+//        int num2 = 2;
+//        int num1 = 1;
+//
+//        listaInteiros.add(num3);
+//        listaInteiros.add(num2);
+//        listaInteiros.add(num1);
+//
+//        System.out.println("Lista de inteiros:");
+//        for (Integer numero : listaInteiros) {
+//            System.out.println(numero);
+//        }
+//
+//        System.out.println("Lista de inteiros ordem crescente:");
+//        Collections.sort(listaInteiros);
+//
+//        for (Integer numero : listaInteiros) {
+//            System.out.println(numero);
+//        }
+
+    }
+}
